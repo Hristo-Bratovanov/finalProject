943282f4cf17ccfe0284f3dbb447db634583d837
Hristo-Bratovanov <hristo_bratovanov@outlook.com>
2025-02-08 20:02:40 +0100

    PermissionsMixin, Forms and Registration

--- accounts/models/app_user.py
+++ accounts/models/app_user.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 from accounts.managers import AppUserManager
 
 
-class AppUser(AbstractBaseUser):
+class AppUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
