e14e929a1eede96f63ac20f704cef3c22cdf0b9f
Hristo-Bratovanov <hristo_bratovanov@outlook.com>
2025-08-13 21:26:01 +0200

    updated urlpatterns

--- finalProject/urls.py
+++ finalProject/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -28,6 +30,7 @@ urlpatterns = [
     path('employees/', include('employees.urls')),
 ]
 
-if not settings.DEBUG:
+debug_env = os.getenv('DEBUG', 'False').lower()
+if debug_env == 'false':
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
