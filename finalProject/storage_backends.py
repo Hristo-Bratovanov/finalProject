852db0b0ee5cb051d26d58e50def822cb6f40409
Hristo-Bratovanov <hristo_bratovanov@outlook.com>
2025-09-08 21:25:21 +0200

    storage_backends

--- finalProject/storage_backends.py
+++ finalProject/storage_backends.py
@@ -1,9 +1,9 @@
 from storages.backends.azure_storage import AzureStorage
-from django.conf import settings
 
 class AzureMediaStorage(AzureStorage):
-    account_name = settings.AZURE_ACCOUNT_NAME
-    account_key = settings.AZURE_ACCOUNT_KEY
-    azure_container = settings.AZURE_CONTAINER
-    expiration_secs = None  # permanent URLs
+    account_name = None
+    account_key = None
+    azure_container = None
+    expiration_secs = None
+
 
