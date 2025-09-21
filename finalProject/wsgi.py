327133186745d8f1a11dbe92f548afca2c139218
Hristo-Bratovanov <hristo_bratovanov@outlook.com>
2025-09-21 10:27:28 +0200

    update wsgi

--- finalProject/wsgi.py
+++ finalProject/wsgi.py
@@ -10,8 +10,6 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'finalProject.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'finalProject.settings'
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finalProject.settings')
 
 application = get_wsgi_application()
